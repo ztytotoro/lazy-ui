b6b41011570fe138d97799112687080abda65564
Yarn <ztytotoro@outlook.com>
2018-05-25 14:26:03 +0800

    add background

---
+++ src/components/background/src/main.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="lz-bg" style="style"></div>
+</template>
+
+<script>
+    export default {
+        name: "LzBg",
+        props: {
+            color: {
+                type: String,
+                default: "white"
+            },
+            image: String
+        },
+        computed: {
+            style: function(){
+                return {
+                    backgrondColor: this.color,
+                    backgroundImage: `url(${this.image})`
+                }
+            }
+        }
+    };
+</script>
+
+<style>
+    .lz-bg {
+        z-index: -10000;
+        position: absolute;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        right: 0;
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
 export { default as Button } from "./components/button/index.js";
 export { default as Container } from "./components/container/index.js";
 export { default as Card } from "./components/card/index.js";
+export { default as Background } from "./components/background/index.js";
