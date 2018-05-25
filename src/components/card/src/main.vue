ce4048bddfcb5fa6f33f7cffe8b80dcd63394b4a
Yarn <ztytotoro@outlook.com>
2018-05-25 13:40:33 +0800

    update card

--- src/components/card/src/main.vue
+++ src/components/card/src/main.vue
@@ -1,5 +1,5 @@
 <template functional>
-    <div class="lz-card">
+    <div class="lz-card" v-bind="data.attrs" v-on="listeners">
         <slot></slot>
     </div>
 </template>
@@ -15,8 +15,9 @@
         background: #fff;
         border-radius: 1px;
         min-height: 80px;
-        min-width: 200px; 
+        min-width: 200px;
         padding: 12px;
         box-shadow: 0px 0px 8px 2px #d0d3da;
+        display: flex;
     }
 </style>
