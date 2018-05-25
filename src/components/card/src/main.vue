7a6b81ab08d135dbb23df51cc74b918b7d10bfbe
Yarn <ztytotoro@outlook.com>
2018-05-25 11:31:03 +0800

    update container

---
+++ src/components/card/src/main.vue
@@ -0,0 +1,107 @@
+<template functional>
+    <button :class="['lz-btn','lz-btn-' + type]" @click="Click" :disabled="disabled">
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+    export default {
+        name: "LzCard",
+
+        props: {
+            type: {
+                default: "default",
+                type: String
+            },
+            disabled: {
+                type: boolean,
+                default: false
+            }
+        },
+
+        methods: {
+            Click: function (e) {
+                this.$emit('click', e);
+            }
+        }
+    };
+</script>
+
+<style>
+    .lz-btn {
+        width: 92px;
+        height: 32px;
+        line-height: 32px;
+        padding: 0;
+        font-weight: normal;
+        outline: none;
+        text-align: center;
+        font-size: 14px;
+        cursor: pointer;
+        touch-action: manipulation;
+        white-space: nowrap;
+        border-radius: 4px;
+    }
+
+    .lz-btn+.lz-btn {
+        margin-left: 10px;
+    }
+
+    .lz-btn-default {
+        border: 1px solid #bfbfbf;
+        background-color: #fff;
+    }
+
+    .lz-btn-default:hover {
+        border-color: #4d99e0;
+        color: #4d99e0;
+    }
+
+    .lz-btn-default:active {
+        background-color: #1c699f;
+        color: #fff;
+    }
+
+    .lz-btn-primary {
+        background-color: #4d99e0;
+        border: 1px solid #4d99e0;
+        color: #fff
+    }
+
+    .lz-btn-primary:hover {
+        background-color: #217ebd;
+        color: #fff;
+    }
+
+    .lz-btn-primary:active {
+        background-color: #1c699f;
+        color: #fff;
+    }
+
+    .lz-btn-text {
+        border-color: transparent;
+        color: #409eff;
+        background: transparent;
+        padding-left: 0;
+        padding-right: 0;
+        width: unset;
+    }
+
+    .lz-btn-text:focus {
+        color: #66b1ff;
+        border-color: transparent;
+        background-color: transparent;
+    }
+
+    .lz-btn:disabled {
+        color: #737477;
+        border: 1px solid #bfbfbf;
+        background-color: #e6e6e6;
+        cursor: default;
+    }
+
+    .lz-btn-text:disabled {
+        background: transparent;
+        border: none;
+    }
+</style>
---
+++ src/components/container/src/main.vue
@@ -0,0 +1,61 @@
+<template functional>
+    <div :class="tClass">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "LzContainer",
+        props: {
+            "-": {
+                type: String,
+                default: "center"
+            },
+            "|": {
+                type: String,
+                default: "center"
+            }
+        },
+        computed: {
+            tClass: function(){
+                let t = ["lz-container"];
+                switch(this["-"]){
+                    case "center": t.push("lz-container--hcenter");break;
+                    case "left": t.push("lz-container--left");break;
+                    case "right": t.push("lz-container--right");break;
+                    default:  break;
+                }
+                switch(this["|"]){
+                    case "center": t.push("lz-container--vcenter");break;
+                    case "top": t.push("lz-container--top");break;
+                    case "bottom": t.push("lz-container--bottom");break;
+                    default: break;
+                }
+            }
+        }
+    };
+</script>
+
+<style>
+    .lz-container {
+        display: flex;
+        padding: 12px;
+    }
+
+    .lz-container--vcenter {
+        align-content: center;
+    }
+
+    .lz-container--hcenter {
+        justify-content: center;
+    }
+
+    .lz-container--left,.lz-container--top {
+        justify-content: flex-start;
+    }
+
+    .lz-container--right,.lz-container--bottom {
+        justify-content: flex-end;
+    }
+</style>
