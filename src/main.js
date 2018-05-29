e4898b9699d511207a14a5eb697ec6e332ae6061
Yarn <ztytotoro@outlook.com>
2018-05-29 16:56:53 +0800

    add input

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 export { default as Button } from "./components/button/index.js";
+export { default as Input } from "./components/input/index.js";
 export { default as Container } from "./layout/container/index.js";
 export { default as Card } from "./components/card/index.js";
 export { default as Background } from "./components/background/index.js";
