168f2e38086e7a13dc5259f7cba6ad6eff42cc18
Yarn <ztytotoro@outlook.com>
2018-05-30 20:45:06 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,6 +1,26 @@
-export { default as Button } from "./components/button/index.js";
-export { default as Input } from "./components/input/index.js";
-export { default as Container } from "./layout/container/index.js";
-export { default as Card } from "./components/card/index.js";
-export { default as Background } from "./components/background/index.js";
-export { default as Row } from "./layout/row/index.js";
+// export { default as Button } from "./components/button/index.js";
+// export { default as Input } from "./components/input/index.js";
+// export { default as Container } from "./layout/container/index.js";
+// export { default as Card } from "./components/card/index.js";
+// export { default as Background } from "./components/background/index.js";
+// export { default as Row } from "./layout/row/index.js";
+
+import Button from "./components/button/index.js";
+import Input from "./components/input/index.js";
+import Container from "./components/container/index.js";
+import Card from "./components/card/index.js";
+import Background from "./components/background/index.js";
+import Row from "./components/background/index.js";
+
+const components = [
+	Button,
+	Input,
+	Container,
+	Card,
+	Background,
+	Row
+];
+
+module.exports = {
+    Button
+}
