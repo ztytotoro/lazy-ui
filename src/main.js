e73b80d78d4c8e5773d175a4de2a8c69d2024bf4
Yarn <ztytotoro@outlook.com>
2018-05-31 14:44:16 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,16 +1,9 @@
-// export { default as Button } from "./components/button/index.js";
-// export { default as Input } from "./components/input/index.js";
-// export { default as Container } from "./layout/container/index.js";
-// export { default as Card } from "./components/card/index.js";
-// export { default as Background } from "./components/background/index.js";
-// export { default as Row } from "./layout/row/index.js";
-
 import Button from "./components/button/index.js";
 import Input from "./components/input/index.js";
-import Container from "./components/container/index.js";
+import Container from "./layout/container/index.js";
 import Card from "./components/card/index.js";
 import Background from "./components/background/index.js";
-import Row from "./components/background/index.js";
+import Row from "./layout/background/index.js";
 
 const components = [
 	Button,
@@ -21,6 +14,18 @@ const components = [
 	Row
 ];
 
+const install = function(Vue) {
+	components.map(component => {
+		component.install(Vue);
+	});
+};
+
 module.exports = {
-    Button
-}
+	install,
+	Button,
+	Input,
+	Container,
+	Card,
+	Background,
+	Row
+};
