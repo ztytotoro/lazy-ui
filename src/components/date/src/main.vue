4bff2f1f5e7646ae0f1febdd3957103d570f2a7b
Yarn <ztytotoro@outlook.com>
2018-06-01 20:35:49 +0800

    update

---
+++ src/components/date/src/main.vue
@@ -0,0 +1,35 @@
+<template>
+    <el-date-picker class="lz-date" :type="type" placeholder :value="value" @change="$emit('input', $event)" style="width:200px" :value-format="format" :disabled="disabled"></el-date-picker>
+</template>
+
+<script>
+    import {DatePicker} from "element-ui";
+    export default {
+        name: "LzDate",
+        props: {
+            value: String,
+            type: {
+                type: String,
+                default: "date"
+            },
+            format: {
+                type: String,
+                default: "yyyy-MM-dd HH:mm:ss"
+            }
+        },
+
+        watch: {
+            data: function () {
+                if (this.data) {
+                    if (this.data.includes("T")) {
+                        this.thisData = this.data.replace("T", " ")
+                    }
+                }
+            }
+        },
+    }
+</script>
+
+<style lang="less">
+    
+</style>
