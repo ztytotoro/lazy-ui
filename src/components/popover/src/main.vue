5b371d21a005fa647057437cea59ce64219a8910
Yarn <ztytotoro@outlook.com>
2018-06-04 18:00:23 +0800

    update

---
+++ src/components/popover/src/main.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="lz-popver">
+        <transition name="fade-in-linear" >
+            <div class="lz-popver-main" v-show="show">
+                <slot></slot>
+            </div>
+        </transition>
+        <slot name="trigger"></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "LzPopover",
+    data() {
+        return {
+            show: false
+        }
+    }
+}
+</script>
